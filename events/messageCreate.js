403b5ea55678387810e79821b05846b45a46abcb
Jan Groß <git@minzkraut.com>
2023-11-07 15:49:30 +0100

    Fixed message log guild name ref

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,6 +3,6 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
-		console.log(`[${message.channel.guild.name}/${message.channe.name}]${message.author.globalName}: ${message.content}`);
+		console.log(`[${message.guild.name}/${message.channe.name}]${message.author.globalName}: ${message.content}`);
 	},
 };
